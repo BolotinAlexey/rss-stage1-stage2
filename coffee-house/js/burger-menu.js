e9123bb5af9f643abc0b17b6a6c8155f682052ed
Oleksii Bolotin <samkiral73@gmail.com>
2023-12-05 22:34:32 +0100

    feat: add handler and styles mobile menu

--- coffee-house/js/burger-menu.js
+++ coffee-house/js/burger-menu.js
@@ -1,9 +1,12 @@
 import getRefs from "./getRefs.js";
 
 const { burger, mobileMenu } = getRefs();
-burger.addEventListener("click", () => {
+burger.addEventListener("click", onBurger);
+mobileMenu.addEventListener("click", onBurger);
+
+function onBurger(e) {
+  console.log(e.target);
+  if (e.target.tagName === "NAV" || e.target.tagName === "UL") return;
   burger.classList.toggle("enableMenu");
   mobileMenu.classList.toggle("enableMenu");
-});
-
-// function onBurger() {}
+}
