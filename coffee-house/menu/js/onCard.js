8d7fba7556d229040c14040d67f8579d266f002f
Oleksii Bolotin <samkiral73@gmail.com>
2023-12-11 11:47:48 +0100

    feat: realesed close modal window finctional

--- coffee-house/menu/js/onCard.js
+++ coffee-house/menu/js/onCard.js
@@ -10,4 +10,16 @@ export default function onCard(e, itemsArray) {
   const index = itemsArray.findIndex((el) => li.dataset.name === el.name);
   backdrop.classList.add("visible");
   backdrop.innerHTML = showModal({ ...itemsArray[index], index });
+  backdrop.addEventListener("click", onBackdrop);
+}
+
+function onBackdrop(e) {
+  console.log(e.target);
+  if (
+    e.target === e.currentTarget ||
+    e.target.classList.contains("modal__btn")
+  ) {
+    backdrop.removeEventListener("click", onBackdrop);
+    backdrop.classList.remove("visible");
+  }
 }
