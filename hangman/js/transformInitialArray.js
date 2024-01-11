ec7dab7c4f87d8a206cb8fefc502f370ecfaf37e
Oleksii Bolotin <samkiral73@gmail.com>
2024-01-11 22:09:35 +0100

    fix: transformInitialArray return array, where answers have not unacceptable symbols

--- hangman/js/transformInitialArray.js
+++ hangman/js/transformInitialArray.js
@@ -1,9 +1,16 @@
+import alphaArr from "../assets/alpha.js";
 import initialArray from "../assets/questionAnswerSet.js";
 
 export default function transformInitialArray() {
   return initialArray
     .filter(
-      (el) => !el[el.answer].includes(" ") && isNaN(parseInt(el[el.answer]))
+      (el) =>
+        !el[el.answer].includes(" ") &&
+        isNaN(parseInt(el[el.answer])) &&
+        el[el.answer]
+          .toUpperCase()
+          .split("")
+          .every((char) => alphaArr.includes(char))
     )
     .map((el) => ({
       question: el.question,
