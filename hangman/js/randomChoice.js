457e221e2af0e1f7b592c27e93e33467d5597336
Oleksii Bolotin <samkiral73@gmail.com>
2024-01-12 11:42:55 +0100

    feat: add favicons and increse widt body parts

--- hangman/js/randomChoice.js
+++ hangman/js/randomChoice.js
@@ -3,6 +3,5 @@ import transformInitialArray from "./transformInitialArray.js";
 export default function randomChoice() {
   const qaArr = transformInitialArray();
   const rndNumber = Math.floor(Math.random() * qaArr.length);
-  console.log(qaArr[rndNumber]);
   return qaArr[rndNumber];
 }
