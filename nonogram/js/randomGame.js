3e6eb25f723eb828c6716ad0719242e7b8f6a33c
Oleksii Bolotin <samkiral73@gmail.com>
2024-01-30 20:07:26 +0100

    refactor: delete magic number in randomeGame()

--- nonogram/js/randomGame.js
+++ nonogram/js/randomGame.js
@@ -1,17 +1,22 @@
 import { listNonograms } from "../assets/listNonograms.js";
 
 export default function randomGame(current) {
-  const randomFolderIndex = Math.floor(Math.random() * 3);
-  const randomNameIndex = Math.floor(Math.random() * 5);
-
+  const randomFolderIndex = Math.floor(
+    Math.random() * Object.keys(listNonograms).length
+  );
   const folder = Object.keys(listNonograms)[randomFolderIndex];
 
+  const randomNameIndex = Math.floor(
+    Math.random() * listNonograms[folder].length
+  );
   const nonogramName = listNonograms[folder][randomNameIndex];
+
   if (
     current &&
     current.folder === folder &&
     current.nonogramName === nonogramName
   )
     randomGame(current);
+
   return { folder, nonogramName };
 }
