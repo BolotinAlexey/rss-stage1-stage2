69627bc4a5545c520461b34a268cb59ec5faa209
Oleksii Bolotin <samkiral73@gmail.com>
2024-02-24 23:52:35 +0100

    feat: add interfaces ResponseNews & Source

---
+++ news-api/src/types/index.ts
@@ -0,0 +1,14 @@
+export default interface ResponseNews {
+    status: 'ok' | 'error'; //If the request was successful or not. Options: ok, error. In the case of error a code and message property will be populated.
+    sources: Source[]; //The results of the request.
+}
+
+interface Source {
+    id: string; // The identifier of the news source. You can use this with our other endpoints.
+    name: string; //The name of the news source
+    description: string; //A description of the news source
+    url: string; //The URL of the homepage.
+    category: string; //The type of news to expect from this news source.
+    language: string; //The language that this news source writes in.
+    country: string; //The country this news source is based in (and primarily writes about).
+}
