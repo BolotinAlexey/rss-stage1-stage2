ddfa35d90e1c8cc075cb031cff08401073af6c9c
Oleksii Bolotin <samkiral73@gmail.com>
2024-02-25 14:36:18 +0100

    feat: add interface Articles

--- news-api/src/types/index.ts
+++ news-api/src/types/index.ts
@@ -1,6 +1,12 @@
-export default interface ResponseNews {
+export interface ResponseNews {
     status: 'ok' | 'error'; //If the request was successful or not. Options: ok, error. In the case of error a code and message property will be populated.
     sources: Source[]; //The results of the request.
+}
+
+export interface Articles {
+    status: Pick<ResponseNews, 'status'>;
+    totalResults: number; //The total number of results available for your request. Only a limited number are shown at a time though, so use the page parameter in your requests to page through them.
+    articles: NewsArticle[]; // The results of the request.
 }
 
 interface Source {
@@ -11,4 +17,20 @@ interface Source {
     category: string; //The type of news to expect from this news source.
     language: string; //The language that this news source writes in.
     country: string; //The country this news source is based in (and primarily writes about).
+}
+
+interface SourceArticle {
+    id: string | null; // The identifier for the source this article came from.
+    name: string; //The name for the source this article came from.
+}
+
+interface NewsArticle {
+    source: SourceArticle; //The identifier id and a display name name for the source this article came from.
+    author: string; //The author of the article
+    title: string; //The headline or title of the article.
+    description: string; //A description or snippet from the article.
+    url: string; //The direct URL to the article.
+    urlToImage: string; //The URL to a relevant image for the article.
+    publishedAt: string; //The date and time that the article was published, in UTC (+000)
+    content: string; //The unformatted content of the article, where available. This is truncated to 200 chars.
 }
