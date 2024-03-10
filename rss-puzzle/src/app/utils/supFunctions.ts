4df7c87cc5bf5ea0d4671ae2fcfc9d422807a240
Oleksii Bolotin <samkiral73@gmail.com>
2024-03-10 14:49:00 +0100

    feat: add base.ts and supFunctions.ts

---
+++ rss-puzzle/src/app/utils/supFunctions.ts
@@ -0,0 +1,37 @@
+export function assertIsDefined<T>(value: T): asserts value is NonNullable<T> {
+  if (value === undefined) {
+    throw new Error(`${value} is not defined`);
+  }
+  if (value === null) {
+    throw new Error(`${value} is null`);
+  }
+}
+
+export function getElement<T extends HTMLElement>(
+  parent: HTMLElement,
+  selector: string,
+): T {
+  const el: T | null = parent.querySelector<T>(selector);
+  assertIsDefined(el);
+  return el;
+}
+export function getElementDocument<T extends HTMLElement>(selector: string): T {
+  const el: T | null = document.querySelector<T>(selector);
+  assertIsDefined(el);
+  return el;
+}
+
+export function createElement(
+  tag: string,
+  cls?: string,
+  content?: string,
+): HTMLElement {
+  const el: HTMLElement = document.createElement(tag);
+  el.innerHTML = content ? content : "";
+  cls && el.classList.add(cls);
+  return el;
+}
+
+export function isHTML(param: unknown): param is HTMLElement {
+  return param instanceof HTMLElement;
+}
