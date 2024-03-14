9296dfe527c6cae0b95e930b1553d46eca40d2c0
Oleksii Bolotin <samkiral73@gmail.com>
2024-03-14 11:18:29 +0100

    fix: RSS-PZ-07 Change 'offers' to 'sentences' in welcomeMarkup and manage game page

--- rss-puzzle/src/app/pages/game/startGame.ts
+++ rss-puzzle/src/app/pages/game/startGame.ts
@@ -1,1 +1,8 @@
-export default function startGame() {}
+import hideWelcomePage from "../../services/hideWelcomePage";
+import renderGame from "../../services/renderGame";
+
+export default function startGame() {
+  hideWelcomePage();
+  const gameMaekup: HTMLElement | null = document.querySelector(".game");
+  if (!gameMaekup) renderGame();
+}
