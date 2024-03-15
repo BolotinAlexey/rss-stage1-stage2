723a729ca9ecfc95250f5929a5b0fec0049b177f
Oleksii Bolotin <samkiral73@gmail.com>
2024-03-15 22:44:32 +0100

    feat: RSS-PZ-09 Add resize for puzzles

--- rss-puzzle/src/app/utils/createPuzzleElementsts.ts
+++ rss-puzzle/src/app/utils/createPuzzleElementsts.ts
@@ -10,16 +10,19 @@ export default function createPuzzleElements(
   const countChars = imgWords.length * 2 + imgWords.join("").length;
   const size = (dataBlock.clientWidth * KOEF_EXT) / countChars;
 
-  const line = imgWords.reduce((a: HTMLElement[], word: string, i: number) => {
-    const node = createElement("div", ["puzzle", "puzzle-data"], word);
-    node.dataset.id = i.toString();
-    node.dataset.word = word;
-    node.style.order = newOrder[i].toString();
-    node.style.paddingLeft = `${size}px`;
-    node.style.paddingRight = `${size}px`;
-    node.style.fontSize = `${size}px`;
-    return [...a, node];
-  }, []);
+  const line: HTMLElement[] = imgWords.reduce(
+    (a: HTMLElement[], word: string, i: number) => {
+      const node = createElement("div", ["puzzle", "puzzle-data"], word);
+      node.dataset.id = i.toString();
+      node.dataset.word = word;
+      node.style.order = newOrder[i].toString();
+      node.style.paddingLeft = `${size}px`;
+      node.style.paddingRight = `${size}px`;
+      node.style.fontSize = `${size}px`;
+      return [...a, node];
+    },
+    [],
+  );
   dataBlock.append(...line);
   return line;
 }
--- rss-puzzle/src/app/utils/createPuzzles.ts
+++ rss-puzzle/src/app/utils/createPuzzles.ts
@@ -1,17 +1,21 @@
+import getElementsResultBlock from "../services/getElementsResultBlock";
 import createPuzzleElements from "./createPuzzleElementsts";
 import { getElementDocument } from "./supFunctions";
 
 const KOEF_EXT = 1.3;
 
-export default function createPuzzles(
-  imgWords: string[],
-  dataBlock: HTMLElement,
-  resultBlock: HTMLElement,
-) {
-  // const newOrder: number[] = randomOrder(imgWords.length);
+export default function createPuzzles(imgWords: string[]) {
+  const { resultBlock, dataBlock } = getElementsResultBlock();
   const countChars = imgWords.length * 2 + imgWords.join("").length;
   const size = (dataBlock.clientWidth * KOEF_EXT) / countChars;
-  const line = createPuzzleElements(imgWords, dataBlock);
+  const puzzleList: NodeList = document.querySelectorAll(".puzzle");
+  const line = puzzleList.length
+    ? Object.values(puzzleList).reduce(
+        (a: HTMLElement[], b: Node) =>
+          b instanceof HTMLElement ? [...a, b] : a,
+        [],
+      )
+    : createPuzzleElements(imgWords, dataBlock);
 
   const paddingY = (resultBlock.clientHeight / 10 - line[0].clientHeight) / 2;
   const totalLength = line.reduce((a: number, el: HTMLElement) => {
@@ -21,7 +25,6 @@ export default function createPuzzles(
 
   imgWords.forEach((word: string) => {
     const el = getElementDocument(`[data-word="${word}"]`);
-    // el.style.order = newOrder[i].toString();
     el.style.padding = `${paddingY}px ${size * coef - 1}px`;
     el.style.fontSize = `${size * coef}px`;
   }, []);
