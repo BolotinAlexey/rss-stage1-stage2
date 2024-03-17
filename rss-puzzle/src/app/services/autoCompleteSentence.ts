b4a8dd005d8d42f63b3df45f1824cb6e3957aebc
Oleksii Bolotin <samkiral73@gmail.com>
2024-03-18 00:49:28 +0100

    feat: RSS-PZ-14 add reset check styles when press autocomplete

--- rss-puzzle/src/app/services/autoCompleteSentence.ts
+++ rss-puzzle/src/app/services/autoCompleteSentence.ts
@@ -1,6 +1,7 @@
 import getCurrentLineElement from "./getCurrentLineElement";
 import getElementsBtns from "./getElementsBtns";
 import getElementsResultBlock from "./getElementsResultBlock";
+import resetCheckStyles from "./resetCheckStyles";
 
 export default function autoCompleteSentence() {
   const { autoCompleteBtn, checkBtn, continueBtn } = getElementsBtns();
@@ -11,13 +12,14 @@ export default function autoCompleteSentence() {
   const { dataBlock } = getElementsResultBlock();
   const puzzles: NodeList = document.querySelectorAll(".puzzle-data");
   const currentLineEl = getCurrentLineElement();
+  resetCheckStyles(currentLineEl);
+
   Object.values(puzzles)
     .sort((a: Node, b: Node) => {
       if (!(a instanceof HTMLElement) || !(b instanceof HTMLElement))
         throw new Error(`${a} or ${b} isn't HTMLElement`);
       if (!a.dataset.id || !b.dataset.id)
         throw new Error(`${a} dataset.id or ${b} dataset.id undefined`);
-
       return Number(a.dataset.id) - Number(b.dataset.id);
     })
     .forEach((el: Node) => {
