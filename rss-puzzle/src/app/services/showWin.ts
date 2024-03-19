a3a0559c3bdecc474c6a1b31c39b672cac82dc35
Oleksii Bolotin <samkiral73@gmail.com>
2024-03-19 11:08:57 +0100

    feat: RSS-PZ-21 Add logic appereance/hiden of volume icon

--- rss-puzzle/src/app/services/showWin.ts
+++ rss-puzzle/src/app/services/showWin.ts
@@ -7,11 +7,15 @@ import readDataSetResultBlock from "./readDataSetResultBlock";
 
 export default function showWin() {
   const hintSentence = getElementDocument(".result-block__hint");
+  const volume = getElementDocument(".button-volume");
   const { resultBlock } = getElementsResultBlock();
-  const { lamp } = getInputsElements();
+  const { lamp, sound } = getInputsElements();
   const nextLine: number = Number(readDataSetResultBlock("currentLine"));
   resultBlock.style.paddingBottom = `${(resultBlock.clientHeight * (COUNT_LINES_PER_RESULT_BLOCK - 1 - nextLine)) / 10}px`;
   changeDataPuzzles();
+
   if (!lamp.checked && !hintSentence.classList.contains("show-hint"))
     hintSentence.classList.add("show-hint");
+  if (!sound.checked && !volume.classList.contains("show-hint"))
+    volume.classList.add("show-hint");
 }
