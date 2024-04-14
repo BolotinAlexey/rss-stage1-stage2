631f4d7cee44d3ccba33bde0c28b01787b3a4c8f
Oleksii Bolotin <samkiral73@gmail.com>
2024-04-14 17:24:32 +0200

    fix: delete from baseUrl /bolotinalexey-JSFE2023Q4/fun-chat/

--- fun-chat/src/app/components/router/onLocation.ts
+++ fun-chat/src/app/components/router/onLocation.ts
@@ -1,15 +1,17 @@
+import { BASE_URL } from "../../constants/index";
 import loginPage from "../../pages/login/index";
 import routePaths from "./routhePaths";
 
 export default function onLocation() {
   // const path: string = window.location.pathname;
   // const body = getElementDocument("body");
-  let path: string = window.location.pathname;
-  console.log(path);
+  const fullPath: string = window.location.pathname;
+  let path: string = fullPath.replace(BASE_URL, "");
+  console.log(path + "ds");
   if (!routePaths.includes(path)) return;
-  if (path === "/") path = "/auth";
-  window.history.pushState({}, "", path);
-  console.log(path);
+  if (path === "/") path = `/auth`;
+  window.history.pushState({}, "", fullPath);
+  console.log(path + "aa");
 
   switch (path) {
     case "/auth":
